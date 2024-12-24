5b9105b38d4b2141bcc980f0d6067854ea61aefa
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-12-24 20:35:31 +0530

    code updated

---
+++ DSA In JS/Strings.js/stringQuestions.js
@@ -0,0 +1,93 @@
+/*
+Truncate the text
+Write a function called truncate that checks the length of a given string - str, and if it surpasses a specified maximum length, maxlength, it replaces the end of the sting with the character "..." so that the length matches the maxium length
+
+input: str = I am prashant
+output: 'prashant'
+
+*/
+
+function truncate(str,maxlength){
+    if(str.length > maxlength){
+        return str.slice(0,maxlength) + "...";
+    }else{
+        return str;
+    }
+}
+
+// console.log(truncate("I am Prashant",5));
+
+
+// ------------------------------
+
+// Q - Palindrome number
+
+// input x = 121 y= 121
+
+// o/p - true
+
+const isPalindrome = function(x){
+
+    // if(x < 0) return false;
+
+    return x<0 ? false: x === +x.toString().split("").reverse().join("");
+}
+
+const res = isPalindrome(111);
+// console.log(res);
+
+
+// ---------------------------------------
+
+// Q -> given two strings x and y, return the hamming distance between them.
+
+//  x = "hello", y = "hwllr"
+
+function hammingDistance(x,y){
+    if(x.length !== y.length){
+        throw new Error("Strings must be of same length")
+    }
+
+    let distance = 0;
+
+    for(let i=0; i<x.length; i++){
+        if(x[i] !== y[i]){
+            distance++;
+        }
+    }
+    return distance
+}
+
+// console.log(hammingDistance("hellow","hwllrw"));
+
+
+//Q- Anagram or not
+
+const isAnagram = function (s,t){
+    s = s.split("").sort().join("");
+    t = t.split("").sort().join("");
+
+    return s===t;
+}
+
+
+const isAnagram2 = function(s,t){
+    if(s.length != t.length) return false;
+
+    let obj1 = {};
+    let obj2 = {};
+
+    for(let i=0; i<s.length; i++){
+        obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+        obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+    }
+
+    for(const key in obj1){
+        if(obj1[key] !== obj2[key]){
+            return false
+        }
+    }
+    return true;
+}
+// console.log(isAnagram('anagram','nagaram'));
+console.log(isAnagram2('anagram','nagaram'));
---
+++ DSA In JS/Strings.js/strings.js
@@ -0,0 +1,90 @@
+// const name = new String("Hi I am Prashant!")
+
+// console.log(name);
+
+
+// const string1 = "This is string";
+
+// const string2 = `Thi is another string `
+
+// console.log(string2);
+
+// const str = 'Prashant'
+// let day = 'Monday'
+
+// console.log(typeof day);
+
+
+// const a = console.log`Hellow ${str} how are you on ${day}`
+
+// console.log(a);
+
+// function test(string,youtubeChannel,person){
+//     console.log(string,youtubeChannel,person);
+//     return;
+// }
+
+
+// const channel = 'prashant malviya'
+// const name = "Prashant"
+
+// test`watch my tutorials on ${channel} run by our orgs, Mr. ${name} is CEO` //o/p [ 'watch my tutorials on ', ' run by our orgs, Mr. ', ' is CEO' ] prashant malviya Prashant
+
+// const accessCharacter = 'this is string';
+
+// console.log(accessCharacter[8]);
+ 
+const str = "Hi I am Krishna"
+
+// console.log(str.replace("s","sh"));
+
+// console.log(str.replaceAll("i","mmm"))
+
+// const concatedStr = str.concat( " How are you today!")
+
+// console.log(concatedStr);
+
+const spacestr = "       this is mohan       ";
+
+// console.log(spacestr.trim())
+
+newStr = "HI am a new string"
+
+// console.log(str.indexOf("is"))
+
+// console.log(newStr.startsWith("t"));
+// console.log(newStr.endsWith("n"));
+
+// console.log(newStr.substring(5,-1));
+
+// console.log(newStr.slice(1,9));
+
+// const tobeconverted = {name:"Radhika"}
+// console.log(String(tobeconverted));
+// console.log(JSON.stringify(tobeconverted));
+
+// console.log(newStr.toLowerCase());
+// console.log(newStr.toUpperCase());
+
+// console.log("A" === "A");
+
+
+// console.log(str.charCodeAt(0));
+// console.log(String.fromCharCode(65));
+
+// const str1 = 'apple hellow'
+// const str2 = 'apple'
+
+// console.log(str1.localeCompare(str2));
+// console.log(str1.includes("hellow"));
+
+// console.log(newStr.split("a"));
+
+
+const fruites = ['apple','banana','grapes']
+console.log(fruites.join(" and "));
+
+
+
+
+
